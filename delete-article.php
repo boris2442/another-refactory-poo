f9cef76f366183c8233d722b7c44e8c72bd5373d
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-08 21:52:36 +0200

    text

--- delete-article.php
+++ delete-article.php
@@ -29,17 +29,15 @@ $pdo = getpdo();
 /**
  * 3. Vérification que l'article existe bel et bien
  */
-$query = $pdo->prepare('SELECT * FROM articles WHERE id = :id');
-$query->execute(['id' => $id]);
-if ($query->rowCount() === 0) {
+$article=findArticle($id);
+if (!$article) {
     die("L'article $id n'existe pas, vous ne pouvez donc pas le supprimer !");
 }
 
 /**
  * 4. Réelle suppression de l'article
  */
-$query = $pdo->prepare('DELETE FROM articles WHERE id = :id');
-$query->execute(['id' => $id]);
+deleteArticle($id);
 
 /**
  * 5. Redirection vers la page d'accueil
--- libraries/database.php
+++ libraries/database.php
@@ -22,3 +22,48 @@ function findAllArticles():array{
 $articles = $resultats->fetchAll();
 return $articles;
 }
+
+function findArticle($id){
+    $pdo=getpdo();
+    $query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
+
+// On exécute la requête en précisant le paramètre :article_id 
+$query->execute(['article_id' => $id]);
+
+// On fouille le résultat pour en extraire les données réelles de l'article
+$article = $query->fetch();
+    return $article;
+}
+
+function findAllComments(int $article_id){
+    $pdo=getpdo();
+$query = $pdo->prepare("SELECT * FROM comments WHERE article_id = :article_id");
+$query->execute(['article_id' => $article_id]);
+$commentaires = $query->fetchAll();
+return  $commentaires;
+}
+
+function deleteArticle($id){
+    $pdo=getpdo();
+    $query = $pdo->prepare('DELETE FROM articles WHERE id = :id');
+$query->execute(['id' => $id]);
+}
+function findComment(int $id){
+    $pdo=getpdo();
+$query = $pdo->prepare('SELECT * FROM comments WHERE id = :id');
+$query->execute(['id' => $id]);
+$comment=$query->fetch();
+return $comment;
+}
+
+function deleteComment(int $id){
+    $pdo=getpdo();
+    $query = $pdo->prepare('DELETE FROM comments WHERE id = :id');
+$query->execute(['id' => $id]);
+}
+
+function insertComment($author, string $content, int $article_id){
+    $pdo=getpdo();
+    $query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
+$query->execute(compact('author', 'content', 'article_id'));
+}
